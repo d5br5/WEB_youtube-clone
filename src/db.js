c611a8943333a8ad04e25e2dec685db66d5a1709
Doh Kim <ehgud456456@naver.com>
2024-11-12 18:27:10 +0900

    Feat: connect to mongo db

---
+++ src/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const DB_PORT = 27017;
+
+const dbURL = `mongodb://127.0.0.1:${DB_PORT}/wetube`;
+
+mongoose.connect(dbURL);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log(`✅ Connected to DB: ${dbURL}`);
+const handleError = (error) => console.log("❌ DB Error", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
