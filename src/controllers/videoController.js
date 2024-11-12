a5d71f703c372391f8cb6258aef1be37aee0e22a
Doh Kim <ehgud456456@naver.com>
2024-11-12 12:39:20 +0900

    Feat: pug conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,32 @@
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.render("watch", { title: "Watch", h1: "Watch Video Page" });
+const fakeUser = {
+  username: "Doh",
+  loggedIn: true,
 };
 
 export const trending = (req, res) => {
-  return res.render("home", { title: "Home", h1: "Welcome Wetube Page!" });
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", {
+    title: "Home",
+    h1: "Welcome Wetube Page!",
+    user: fakeUser,
+    videos,
+  });
+};
+
+export const see = (req, res) => {
+  return res.render("watch", {
+    title: "Watch",
+    h1: "Watch Video Page",
+    user: fakeUser,
+  });
 };
 
 export const edit = (req, res) => {
-  return res.render("edit", { title: "Edit", h1: "Edit your video" });
+  return res.render("edit", {
+    title: "Edit",
+    h1: "Edit your video",
+    user: fakeUser,
+  });
 };
 
 export const remove = (req, res) => {
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { join, login } from "../controllers/userController";
+import { join, login, logout } from "../controllers/userController";
 import { search, trending } from "../controllers/videoController";
 
 const globalRouter = express.Router();
@@ -7,6 +7,7 @@ const globalRouter = express.Router();
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
+globalRouter.get("/logout", logout);
 globalRouter.get("/search", search);
 
 export { globalRouter };
