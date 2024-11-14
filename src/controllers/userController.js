07ea39898aa9f1aab99df5856cb24184a25b0d33
Doh Kim <ehgud456456@naver.com>
2024-11-14 12:03:30 +0900

    feat: normal user join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,27 @@
+import User from "../models/User";
+
+export const login = (req, res) => res.send("Login");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See User");
+
+export const getJoin = (req, res) => {
+  return res.render("join", { title: "Create Account" });
+};
+
+export const postJoin = async (req, res) => {
+  const { email, username, password, name, location } = req.body;
+  await User.create({ email, username, password, name, location });
+  return res.redirect("/login");
+};
+
+export const getLogin = async (req, res) => {
+  const user = await User.find({});
+  console.log(user);
+  return res.render("login", { title: "Login" });
+};
+
+export const postLogin = async (req, res) => {
+  return res.redirect("/");
+};
---
+++ src/routers/rootRouter.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  getJoin,
+  getLogin,
+  logout,
+  postJoin,
+  postLogin,
+} from "../controllers/userController";
+import { search, home } from "../controllers/videoController";
+
+const rootRouter = express.Router();
+
+rootRouter.get("/", home);
+rootRouter.route("/join").get(getJoin).post(postJoin);
+rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.get("/logout", logout);
+rootRouter.get("/search", search);
+
+export { rootRouter };
