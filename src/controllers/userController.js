c3c709e1f4081008a7956c3c4d875c7ec6b9323a
DohyungKim <ehgud456456@naver.com>
2024-11-17 21:18:24 +0900

    feat: save avatar_url with github login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -48,7 +48,7 @@ export const getLogin = async (req, res) => {
 
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render("login", {
       title: "Login",
@@ -133,25 +133,22 @@ export const finishGithubLogin = async (req, res) => {
       return res.redirect("/login");
     }
 
-    const existingUser = await User.findOne({ email: emailObj.email });
-    if (existingUser) {
-      req.session.loggedIn = true;
-      req.session.user = existingUser;
-      return res.redirect("/");
-    } else {
-      const user = await User.create({
+    let user = await User.findOne({ email: emailObj.email });
+
+    if (!user) {
+      user = await User.create({
         email: emailObj.email,
         username: userData.login,
         password: "",
         socialOnly: true,
         name: userData.name,
         location: userData.location,
+        avatarUrl: userData.avatar_url,
       });
-
-      req.session.loggedIn = true;
-      req.session.user = user;
-      return res.redirect("/");
     }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
