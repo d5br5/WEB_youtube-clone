04cd0073e38b033345078d784303f305bb793992
DohyungKim <ehgud456456@naver.com>
2024-11-17 21:33:08 +0900

    feat: logout

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,13 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
 
-export const login = (req, res) => res.send("Login");
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Delete User");
-export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See User");
+
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 
 export const getJoin = (req, res) => {
   return res.render("join", { title: "Create Account" });
