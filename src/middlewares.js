aa1dda7c2642c41973cedaa990e95b58c6e7b6ce
Doh Kim <ehgud456456@naver.com>
2025-01-03 15:38:19 +0900

    feat: protector, public only middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,3 +5,21 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WETUBE";
   next();
 };
+
+// 로그인이 되어있는지 확인하는 미들웨어
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+// 로그인이 되어있지 않은지 확인하는 미들웨어
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
