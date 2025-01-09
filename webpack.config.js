b4def902f549e9f2a8dc1369dbf4425382362c79
Doh Kim <ehgud456456@naver.com>
2025-01-09 16:10:08 +0900

    feat: install scss and set config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 // webpack이 assets/js에 빌드하면, server.js 에서 해당 폴더를 읽어야 한다.
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const path = require("path");
 
 module.exports = {
   entry: "./src/client/js/main.js",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   mode: "production",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
   },
   module: {
     rules: [
@@ -20,6 +27,14 @@ module.exports = {
             presets: [["@babel/preset-env"]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        // sass-loader: scss -> css
+        // css-loader: import 관련문 해석
+        // style-loader: inject css into html
+        // 역순으로 실행됨
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
