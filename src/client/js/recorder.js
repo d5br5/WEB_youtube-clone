5fc9268619696745ccccafc9fc9ad2548510c91b
Doh Kim <ehgud456456@naver.com>
2025-01-13 12:56:24 +0900

    feat: 썸네일 추출해서 다운로드

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,4 +1,4 @@
-import { fetchFile, toBlobURL } from "@ffmpeg/util";
+import { fetchFile } from "@ffmpeg/util";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 
 const recordBtn = document.getElementById("recordBtn");
@@ -61,17 +61,36 @@ const handleDownload = async () => {
   ffmpeg.on("log", ({ type, message }) => console.log(message));
   ffmpeg.writeFile("recording.webm", await fetchFile(videoFile));
   await ffmpeg.exec(["-i", "recording.webm", "-r", "60", "output.mp4"]);
+  await ffmpeg.exec([
+    "-i",
+    "recording.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg",
+  ]);
 
-  const data = await ffmpeg.readFile("output.mp4");
-  const blob = new Blob([data.buffer], { type: "video/mp4" });
+  const mp4File = await ffmpeg.readFile("output.mp4");
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
 
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "MyRecording.mp4";
-  document.body.appendChild(a);
-  a.click();
+  const thumbnailFile = await ffmpeg.readFile("thumbnail.jpg");
+  const thumbnailBlob = new Blob([thumbnailFile.buffer], { type: "image/jpg" });
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
+  const mp4A = document.createElement("a");
+  mp4A.href = mp4Url;
+  mp4A.download = "MyRecording.mp4";
+  document.body.appendChild(mp4A);
+  mp4A.click();
   recordBtn.innerText = "Download Recording";
+
+  const thumbnailUrl = URL.createObjectURL(thumbnailBlob);
+  const thumbnailA = document.createElement("a");
+  thumbnailA.href = thumbnailUrl;
+  thumbnailA.download = "MyThumbnail.jpg";
+  document.body.appendChild(thumbnailA);
+  thumbnailA.click();
 };
 
 recordBtn.addEventListener("click", handleStart);
