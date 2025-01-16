a17e2f473a88db3d368f9ed651868032e61cdedb
Doh Kim <ehgud456456@naver.com>
2025-01-16 11:17:03 +0900

    remove useless code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -135,7 +135,6 @@ export const finishGithubLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   req.session.destroy();
-  req.flash("info", "Not Authorized");
   return res.redirect("/");
 };
 export const getEdit = (req, res) => {
